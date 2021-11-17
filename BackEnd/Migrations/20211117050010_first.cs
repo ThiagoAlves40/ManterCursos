38345276a5ac10a56e6e8424486fb6a9ef75ec9f
Thiago.Alves <thiago.alves@castgroup.com.br>
2021-11-17 03:00:27 -0300

    first commit

---
+++ BackEnd/Migrations/20211117050010_first.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataInicio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataTermino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    QtdAlunos = table.Column<int>(type: "int", nullable: false),
+                    CategoriaFk = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursoId);
+                    table.ForeignKey(
+                        name: "FK_Cursos_Categorias_CategoriaFk",
+                        column: x => x.CategoriaFk,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_CategoriaFk",
+                table: "Cursos",
+                column: "CategoriaFk");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ BackEnd/Models/Curso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEnd.Models
+{
+    public class Curso
+    {
+        [Key]
+        public int CursoId { get; set; }
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        [DataType(DataType.Date), Display(Name = "Data de Início")]
+        public string DataInicio { get; set; }
+
+        [Required]
+        [DataType(DataType.Date), Display(Name = "Data de Término")]
+        public string DataTermino { get; set; }
+
+        public int QtdAlunos { get; set; }
+
+        [Required]
+        public int CategoriaFk { get; set; }
+        [ForeignKey("CategoriaFk")]
+        public Categoria Categoria { get; set; }
+    }
+}
